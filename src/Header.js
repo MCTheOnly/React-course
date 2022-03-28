fd515c3f468beee83418d9ea739cc6b13c932d9a
Martin Chorzewski <mchorzewski@whitelabelcoders.com>
2022-03-28 10:53:39 +0200

    changeState

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,15 @@ const Header = () => {
 	return (
 		<div className="header__wrapper">
 			<h2>Site Logo</h2>
-			<nvv className="header__nav">
+			<nav className="header__nav">
 				<a href="/">About</a>
-				<a href="/">Contact Us</a>
-			</nvv>
+				<a href="/" style={
+					{
+						color: 'red',
+						textDecoration: 'none'
+					}
+				}>Contact Us</a>
+			</nav>
 		</div>
 	);
 }
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,30 @@
+import { useState } from 'react'
+
 const Home = () => {
-	const title = 'React Course'
-	const number = 8
-	const obj = { name: 'name', age: 8 }
+	// const title = 'React Course'
+	// const number = 8
+	// const obj = { name: 'name', age: 8 }
+
+	const [title, setTitle] = useState('React Course')
+
+	const clickHandler = (txt, e) => {
+		console.log(txt, e)
+	}
+
+	const changeState = () => {
+		setTitle('Cool React Course')
+	}
 
 	return (
 		<div className="home">
 			<h1>{title}</h1>
-			<p>{number}</p>
+			<button onClick={(e) => clickHandler('clicked', e)}>Click Event</button>
+			<button onClick={() => changeState()}>Click Event</button>
+
+			{/* <p>{number}</p>
 			<p>{obj.name}</p>
 			<p>{[1, 2, 3, 4, 5]}</p>
-			<p>{Math.random()}</p>
+			<p>{Math.random()}</p> */}
 		</div>
 	);
 }
